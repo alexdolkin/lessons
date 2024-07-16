b8ec586f7d270400828c720ae724e17d6bb5afd8
alexdolkin <162968210+alexdolkin@users.noreply.github.com>
2024-07-16 16:14:07 +0300

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,77 @@
+// Строгий режим
+"use strict"
+
+
+
+// function showMessage() {
+// console.log("Повідомлення");
+// }
+// showMessage();
+
+
+// function showMessage(someText = "Текст за замовченням") {
+// 	console.log(someText);
+// }
+// showMessage()
+
+
+// const numOne = 4
+// const numTwo = 2
+// function calcSum(a = 0, b = 0) {
+// 	console.log(a + b)
+// }
+// calcSum(numOne, numTwo)
+
+// ====================================
+
+// №1
+// let someVar = 0;
+// ++someVar;
+// if (someVar) {
+// 	console.log(someVar);
+// }
+
+// №2
+// let someLine = "Рядок №"
+// for (let i = 1; i < 11; ++i) {
+// 	console.log('Рядок №' + [i])
+// }
+
+// #3
+// if (2 * 20 <= 10 || 30 / 2 < 5 && 10 <= "10" || 20 === "20") {
+// 	console.log('000');
+// }
+
+// #4
+// function divide(a, b) {
+// 	// Перевірка на передані значення і ділення на нуль
+// 	if (typeof a !== 'number', typeof b !== 'number', isNaN(a), isNaN(b), b === 0) {
+// 		return "Результат ділення: некоректні дані";
+// 	}
+
+	// Виконання ділення
+	// let result = a / b;
+
+	// Повернення зядка з результатом
+// 	return "Результат ділення: " + result;
+// }
+
+// Приклади виклику вункції з різними значеннями
+// console.log(divide(10, 2));
+// console.log(divide(10, 0));
+// console.log(divide(10));
+// console.log(divide());
+// console.log(divide(15, 3));
+// console.log(divide("a", 3));
+
+
+
+// #5
+
+// let someArray = [12, "item", 10, "87", true]
+// console.log(someArray);
+// someArray.forEach((item) => {
+// 	if (item === 10) {
+// 		console.log(item);
+// 	  }
+// 	})
